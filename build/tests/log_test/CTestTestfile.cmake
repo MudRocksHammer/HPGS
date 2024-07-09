35ee02692d2f0e0b274d7a38d1ba0e72b9973905
Leesken <82818654+MudRocksHammer@users.noreply.github.com>
2024-07-09 22:14:03 +0800

    Delete build directory

--- build/tests/log_test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xiaorenbo/share/HPGS/tests/log_test
-# Build directory: /home/xiaorenbo/share/HPGS/build/tests/log_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(LOG_TEST "/home/xiaorenbo/share/HPGS/build/tests/log_test/test_log")
-set_tests_properties(LOG_TEST PROPERTIES  _BACKTRACE_TRIPLES "/home/xiaorenbo/share/HPGS/tests/log_test/CMakeLists.txt;8;add_test;/home/xiaorenbo/share/HPGS/tests/log_test/CMakeLists.txt;0;")
