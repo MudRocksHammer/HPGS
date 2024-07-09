adef6441646769df09979082fa490a46c16b30d4
xrb <xiaorenbo730@gmail.com>
2024-07-09 14:08:19 +0000

    test using gtest

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/xiaorenbo/share/HPGS/tests
+# Build directory: /home/xiaorenbo/share/HPGS/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("log_test")
+subdirs("config_test")
---
+++ build/tests/config_test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/xiaorenbo/share/HPGS/tests/config_test
+# Build directory: /home/xiaorenbo/share/HPGS/build/tests/config_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(CONFIG_TEST "/home/xiaorenbo/share/HPGS/build/tests/config_test/test_config")
+set_tests_properties(CONFIG_TEST PROPERTIES  _BACKTRACE_TRIPLES "/home/xiaorenbo/share/HPGS/tests/config_test/CMakeLists.txt;8;add_test;/home/xiaorenbo/share/HPGS/tests/config_test/CMakeLists.txt;0;")
