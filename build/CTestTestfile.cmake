5e91b8e04befaaecb453f95483fd85bc6a1b265a
xrb <xiaorenbo730@gmail.com>
2024-07-10 11:49:44 +0000

    update

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xiaorenbo/share/HPGS
-# Build directory: /home/xiaorenbo/share/HPGS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xiaorenbo/share/HPGS/tests
-# Build directory: /home/xiaorenbo/share/HPGS/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("log_test")
-subdirs("config_test")
